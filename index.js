a2697d3628d692988bf26e618c17fcd7acd1d3cc
Sam <sampat.palani@gmail.com>
2017-02-24 11:34:54 -0800

    Added case incrementer

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-const uscis = require('uscis')
+const uscis = require('uscis');
+let caseStart = 1690517100;
+let caseCenter ='LIN';
+var caseIncrementer;
+let caseTitle = [];
+let caseDetails = [];
 
-uscis('LIN1690517123').then((status) => {
-  console.log(status.title)
-  console.log(status.details)
-})
+for(i=1;i<=9;i++){
+  var caseIncrementer = caseStart+i;
+  var caseNumber = caseCenter+caseIncrementer;
+  uscis(caseNumber).then((status) => {
+    console.log(caseNumber);
+    console.log(status.title)
+    console.log(status.details)
+  })
+}
