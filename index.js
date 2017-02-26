b00308fa25e393d4275095edfbc9385767743a14
Sam <sampat.palani@gmail.com>
2017-02-25 18:24:10 -0800

    fixed callback errors

--- index.js
+++ index.js
@@ -5,12 +5,22 @@ var caseIncrementer;
 let caseTitle = [];
 let caseDetails = [];
 
-for(i=1;i<=9;i++){
+
+for(i=1;i<=20;i++){
   var caseIncrementer = caseStart+i;
   var caseNumber = caseCenter+caseIncrementer;
-  uscis(caseNumber).then((status) => {
-    console.log(caseNumber);
-    console.log(status.title)
-    console.log(status.details)
+  getCaseDetails(caseNumber,function(title,details,number){
+    if (details.indexOf("I-140") >= 0){
+      console.log(number);
+      console.log(title);
+      console.log(details);
+    }
   })
+}
+
+
+function getCaseDetails(caseNumber,callback){
+  uscis(caseNumber).then((status) => {
+      callback(status.title,status.details,caseNumber);
+  });
 }
