c7743c833e17a5b12014e7cd22023a26d8a6d76d
Sam <sampat.palani@gmail.com>
2017-03-28 13:23:16 -0700

    Formatted output

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const uscis = require('uscis');
-let caseStart = 1690517100;
+let caseStart = 1690517200;
 let caseCenter ='LIN';
 var caseIncrementer;
 let caseTitle = [];
 let caseDetails = [];
 
 
-for(i=1;i<=20;i++){
+for(i=1;i<=50;i++){
   var caseIncrementer = caseStart+i;
   var caseNumber = caseCenter+caseIncrementer;
   getCaseDetails(caseNumber,function(title,details,number){
     if (details.indexOf("I-140") >= 0){
-      console.log(number);
-      console.log(title);
-      console.log(details);
+      // console.log(number);
+      // console.log(number);
+      // console.log(details.substr(2, details.indexOf(',')));
+      console.log(number+','+title+','+details.substr(2, details.indexOf(',')));
     }
   })
 }
